dcd1705a5d96dd2215708d39238f0c1b73db743f
adityasugandhi <39901010+adityasugandhi@users.noreply.github.com>
2024-08-03 03:07:57 +0000

    added favicons

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
+
         <meta charSet="UTF-8" />
         <meta name="keywords" content="Aditya Sugandhi, Software Engineer, Data Scientist, Web Development, Researcher" />
         <link rel="canonical" href="https://adityasugandhi.com" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
         {/* Add more site-wide tags here */}
       </Head>
       <body>
